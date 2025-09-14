03ca079dcaf014a8db435164da05549b8fdbddf1
Coyote <jbrimm@mac.com>
2025-09-14 00:06:15 -0700

    feat: add offset method to Position class for coordinate manipulation

--- tui_minesweeper/position.py
+++ tui_minesweeper/position.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
 from typing import NamedTuple
+
 
 class Position(NamedTuple):
     """Immutable coordinate for a cell in the minefield."""
     row: int
     col: int
+
+    def offset(self, other: Position) -> Position:
+        return Position(self.row + other.row, self.col + other.col)
